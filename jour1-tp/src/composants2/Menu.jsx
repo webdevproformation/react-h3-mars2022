83cef5761f654f18bf777888339a9ec8594682a9
Malik <malik.h@webdevpro.net>
2022-03-15 14:54:27 +0100

    menu suite et fin

--- jour1-tp/src/composants2/Menu.jsx
+++ jour1-tp/src/composants2/Menu.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Menu = () => {
     return <nav className="navbar navbar-expand bg-primary navbar-dark px-4">
-        <a href="#" className="navbar-brand">jour2</a>
+        <Link to="/" className="navbar-brand">jour2</Link>
         <ul className="navbar-nav">
             <li className="nav-item">
                 <Link to="/" className="nav-link">Accueil</Link>
